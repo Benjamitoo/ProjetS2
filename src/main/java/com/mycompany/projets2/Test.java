140d8a464dfdde442bab99d1d2ff2f1cad4ffbcd
Thomas CATHERINE <thomas.catherine@insa-strasbourg.fr>
2025-04-02 10:45:11 +0200

    etat initial2

---
+++ src/main/java/com/mycompany/projets2/Test.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projets2;
+
+/**
+ *
+ * @author tcatherine01
+ */
+public class Test {
+    static void test(){
+        System.out.println("Hello test!");
+    }
+    
+}
