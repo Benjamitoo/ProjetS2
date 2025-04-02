bbf2fd35e40fb90b9afa1fbb7d9851994a3a68f6
Thomas CATHERINE thomas.catherine@insa-strasbourg.fr <>
2025-04-02 10:24:24 +0200

    etat initial

---
+++ src/main/java/com/mycompany/projets2/ProjetS2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projets2;
+
+/**
+ *
+ * @author tcatherine01
+ */
+public class ProjetS2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Test.test();
+    }
+}
