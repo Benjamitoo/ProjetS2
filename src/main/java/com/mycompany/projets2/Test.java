735f111e291280987732fe133c7bdfc8d7d53f64
benjamin <benjamin@wifi-etudiants-10-172-78-162.in.insa-strasbourg.fr>
2025-04-24 10:22:02 +0200

    first commit

--- src/main/java/com/mycompany/projets2/Test.java
+++ src/main/java/com/mycompany/projets2/Test.java
@@ -12,7 +12,7 @@ public class Test {
     static void test(){
         System.out.println("Hello test!");
         System.out.println("Goodby test!");
-        System.out.println("Goodby test!!!!!!");
+        System.out.println("Goodby test!!!!");
     }
     
 }
