9ab031a39d26585bc824a45fee8cdfa1aaadd298
benjamin <benjamin@wifi-etudiants-10-172-78-162.in.insa-strasbourg.fr>
2025-05-14 13:46:06 +0200

    Modification valeur package dans code

--- src/main/java/modele/Equipement.java
+++ src/main/java/modele/Equipement.java
@@ -1,13 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modele;
 
-/**
- *
- * @author benjamin
- */
+
+
+
+
+
+
 public class Equipement {
     private String id;
     private String ref;
--- src/main/java/modele/ProjetS2.java
+++ src/main/java/modele/ProjetS2.java
@@ -1,13 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+package modele;
 
-package com.mycompany.projets2;
 
-/**
- *
- * @author tcatherine01
- */
+
+
+
+
+
 public class ProjetS2 {
 
     public static void main(String[] args) {
