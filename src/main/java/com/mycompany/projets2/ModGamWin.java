d73b2cfd28b5bade336b43f47f068495a4b5cafb
CATHERINE-Thomas <thomas.catherine@insa-strasbourg.fr>
2025-05-14 12:24:14 +0200

    prémice d'interface utilisateur (classe Gamme)

---
+++ src/main/java/com/mycompany/projets2/ModGamWin.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projets2;
+
+/**
+ *
+ * @author thoma
+ */
+
+
+	import java.awt.BorderLayout;
+	import java.awt.Color;
+	import java.awt.Font;
+	import java.awt.event.ActionEvent;
+	import java.awt.event.ActionListener; 
+	import javax.swing.JFrame;
+	import javax.swing.JLabel;
+	import javax.swing.JPanel;
+        import javax.swing.JButton;
+
+public class ModGamWin extends JFrame  implements ActionListener{
+		//fenêtre de réponse du mode Lettres d'un joueur
+		
+		
+		private JButton ajout = new JButton("Ajouter");
+                private JButton supp = new JButton("Supprimer");
+                private JLabel titre = new JLabel();
+
+		
+		
+		public ModGamWin(Gamme gamme){             
+                    this.setTitle(gamme.getRef());
+		    this.setSize(600, 300);
+		    this.setLocation(900, 200); 
+                    
+                    this.titre.setText("Machine " + gamme.getRef());
+		    
+	
+		    
+		    Font police = new Font("Tahoma", Font.BOLD, 16);
+			
+						
+		    JPanel pan = new JPanel();
+		    
+		    pan.setBackground(Color.WHITE);
+		    pan.setLayout(new BorderLayout());
+		    pan.add(titre, BorderLayout.NORTH);
+		    		  		    		    		    
+		    JPanel south = new JPanel();
+ 
+		    pan.add(south, BorderLayout.SOUTH);
+                    south.add(ajout);
+                    
+		   
+		    		    		    		    		   		    
+		    this.setContentPane(pan);               
+		    this.setVisible(true);		 
+		}													 		 			
+		 
+		public void actionPerformed(ActionEvent arg0) {
+			//System.out.println(arg0.getSource());
+			
+		 }
+		
+		 
+}
+
+
+
+
